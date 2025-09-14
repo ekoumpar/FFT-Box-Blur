441d0d0ef0ef273c7bfaf701380d5f8633058951
ekoumpar <cdkhelen@gmail.com>
2025-09-14 22:23:16 +0300

    Preallocation of memory

--- box_blur_filter.jl
+++ box_blur_filter.jl
@@ -1,10 +1,124 @@
 module BoxBlur
 
 using FFTW
+import FFTW: mul!
 
-export blur_filter_conv, blur_filter_conv_FFT
+export blur_filter_conv_init, blur_filter_fft_init, blur_filter_conv, blur_filter_FFT
 
-function blur_filter_conv(image::AbstractArray, kernel_size::Int)
+function blur_filter_conv_init(image_dimensions::Tuple, kernel_size::Int)
+
+    """
+        Preallocates the needed arrays for blur_filter_conv function.
+
+        Arguments:
+        - image_dimensions: A tuple specifying the dimensions of the input image.
+        - kernel_size: An odd integer specifying the size of the box filter.
+
+        Returns:
+        - kernel: The box filter kernel.
+        - padded_image: The array of zero-padded image.
+        - blurred_image: The array of output blurred image.
+    """
+
+    pad = div(kernel_size, 2)
+
+    if length(image_dimensions) == 1
+
+        padded_size = (image_dimensions[1] + 2*pad,)
+
+        padded_image  = zeros(Float32, padded_size)
+        kernel = zeros(Float32, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+
+    elseif length(image_dimensions) == 2
+
+        padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad)
+
+        padded_image  = zeros(Float32, padded_size)
+        kernel = zeros(Float32, kernel_size, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+     
+    elseif length(image_dimensions) == 3
+
+        padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad, image_dimensions[3] + 2*pad)
+
+        padded_image  = zeros(Float32, padded_size)
+        kernel = zeros(Float32, kernel_size, kernel_size, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+        
+    else
+        error("Unsupported image dimension")
+    end
+
+    return kernel, padded_image, blurred_image
+
+end
+
+function blur_filter_fft_init(image_dimensions::Tuple, kernel_size::Int)
+    """
+        Preallocates the needed arrays for blur_filter_FFT function.
+
+        Arguments:
+        - image_dimensions: A tuple specifying the dimensions of the input image.
+        - kernel_size: An odd integer specifying the size of the box filter.
+
+        Returns:
+        - kernel: The box filter kernel.
+        - padded_image: The array of zero-padded image.
+        - padded_kernel: The array of zero-padded kernel.
+        - blurred_image: The array of output blurred image.
+        - fft_image: The array of the the padded image FFT.
+        - fft_kernel: The array of the padded kernel FFT.
+        - plan_fwd_image: The FFTW plan for forward FFT of the image.
+        - plan_fwd_kernel: The FFTW plan for forward FFT of the kernel.
+        - plan_inv: The FFTW plan for inverse FFT.
+    """
+
+    pad = div(kernel_size, 2)
+
+    if length(image_dimensions) == 1
+
+        padded_size = (image_dimensions[1] + 2*pad,)
+
+        kernel = zeros(Float32, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+
+    elseif length(image_dimensions) == 2
+
+        padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad)
+
+        kernel = zeros(Float32, kernel_size, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+     
+    elseif length(image_dimensions) == 3
+
+        padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad, image_dimensions[3] + 2*pad)
+
+        kernel = zeros(Float32, kernel_size, kernel_size, kernel_size)
+        blurred_image = zeros(Float32, image_dimensions)
+        
+    else
+        error("Unsupported image dimension")
+    end
+
+    # Allocate padded arrays
+    padded_image  = zeros(Float32, padded_size)
+    padded_kernel = zeros(Float32, padded_size)
+
+    # FFT preplanning
+    plan_fwd_image = plan_rfft(padded_image; flags=FFTW.MEASURE)
+    plan_fwd_kernel = plan_rfft(padded_kernel; flags=FFTW.MEASURE)
+    fft_image  = plan_fwd_image * padded_image
+    fft_kernel = plan_fwd_kernel * padded_kernel
+
+    # Inverse FFT plan
+    plan_inv = plan_irfft(fft_image, padded_size[end]; flags=FFTW.MEASURE)
+
+    return kernel, padded_image, padded_kernel, blurred_image, fft_image, fft_kernel, plan_fwd_image, plan_fwd_kernel, plan_inv
+end
+
+function blur_filter_conv(image::AbstractArray, kernel::AbstractArray, padded_image::AbstractArray, 
+                                                    blurred_image::AbstractArray, kernel_size::Int)
 
     """
         Applies a box blur filter using same convolution, so that edges are included during calculation.
@@ -22,19 +136,14 @@ function blur_filter_conv(image::AbstractArray, kernel_size::Int)
     # to adjust image to include edges in convolution (same convolution)
     # and to show how many neighbours are considered around a pixel
     pad = div(kernel_size, 2) 
-    blurred_image = zeros(Float32, image_dimensions)
-
     
     if length(image_dimensions) == 1    # 1D Images
 
         elements = image_dimensions[1]
 
-        # Create Kernel
-        kernel = ones(Float32, kernel_size) / kernel_size
-
         # Zero padding for including the edges
-        padded_image = zeros(Float32, elements + 2*pad)
-        padded_image[pad+1:end-pad] .= image
+        padded_image[pad+1:end-pad] .= Float32.(image)
+        kernel .= 1/kernel_size
 
         # Calculate convolution
         # For each pixel sum the product of the kernel and the corresponding image patch
@@ -46,18 +155,13 @@ function blur_filter_conv(image::AbstractArray, kernel_size::Int)
             blurred_image[i] = neighbours_mul
         end
 
-        return blurred_image
-
     elseif length(image_dimensions) == 2   # 2D Images
 
         rows, cols = image_dimensions
 
-        # Create Kernel
-        kernel = ones(Float32, kernel_size, kernel_size) / (kernel_size^2)
-
         # Zero padding for including the edges
-        padded_image = zeros(Float32, rows + 2*pad, cols + 2*pad)
-        padded_image[pad+1:end-pad, pad+1:end-pad] .= image
+        padded_image[pad+1:end-pad, pad+1:end-pad] .= Float32.(image)
+        kernel .= 1/(kernel_size^2)
 
         # Calculate convolution
         # For each pixel sum the product of the kernel and the corresponding 2D image patch
@@ -72,18 +176,14 @@ function blur_filter_conv(image::AbstractArray, kernel_size::Int)
                 blurred_image[i, j] = neighbours_mul
             end
         end
-        return blurred_image
 
     elseif length(image_dimensions) == 3   # 3D Images
         
         depth, rows, cols = image_dimensions
 
-        # Create Kernel
-        kernel = ones(Float32, kernel_size, kernel_size, kernel_size) / (kernel_size^3)
-
         # Zero padding for including the edges
-        padded_image = zeros(Float32, depth + 2*pad, rows + 2*pad, cols + 2*pad)
-        padded_image[pad+1:end-pad, pad+1:end-pad, pad+1:end-pad] .= image
+        padded_image[pad+1:end-pad, pad+1:end-pad, pad+1:end-pad] .= Float32.(image)
+        kernel .= 1/(kernel_size^3)
 
         # Calculate convolution
         # For each pixel sum the product of the kernel and the corresponding 3D image patch
@@ -102,14 +202,15 @@ function blur_filter_conv(image::AbstractArray, kernel_size::Int)
                 end
             end
         end
-        return blurred_image
     else
         error("Image dimensions not supported")
     end 
 end
 
 
-function blur_filter_conv_FFT(image::AbstractArray, kernel_size::Int)
+function blur_filter_FFT(image::AbstractArray, kernel::AbstractArray, padded_image::AbstractArray, 
+                                padded_kernel::AbstractArray, blurred_image::AbstractArray, kernel_size::Int,
+                                fft_image::AbstractArray, fft_kernel::AbstractArray, plan_fwd_image::FFTW.Plan, plan_fwd_kernel::FFTW.Plan, plan_inv::FFTW.Plan)
 
     """
         Applies a box blur filter using Fast Fourier Transform (FFT)-based convolution 
@@ -128,86 +229,84 @@ function blur_filter_conv_FFT(image::AbstractArray, kernel_size::Int)
 
     image_dimensions = size(image)
 
+    blurred_image .= 0.0
+    padded_image .= 0.0
+    padded_kernel .= 0.0
+
     if length(image_dimensions) == 1    # 1D Images
-        # Create Kernel
-        kernel = ones(Float32, kernel_size) / kernel_size
 
         # Pad image and kernel to match convolution product
         pad = div(kernel_size, 2)
         padded_size = (image_dimensions[1] + 2*pad,)
 
-        padded_image = zeros(Float32, padded_size)
         padded_image[1+pad:end-pad] .= Float32.(image)
 
-        padded_kernel = zeros(Float32, padded_size)
+        kernel .= 1/kernel_size
         padded_kernel[1:kernel_size] .= kernel
-        padded_kernel = circshift(padded_kernel, -pad)  # center the kernel
+        padded_kernel .= circshift(padded_kernel, -pad)  # center the kernel
 
-        # Calculate real FFTs of image and kernel
-        fft_image = rfft(padded_image)
-        fft_kernel = rfft(padded_kernel)
+        # Forward FFTs
+        mul!(fft_image,  plan_fwd_image, padded_image)
+        mul!(fft_kernel, plan_fwd_kernel, padded_kernel)
 
-        # Calculate convolution via the real reverse FFT
-        fft_image .*= fft_kernel    # same array for lower memory usage
-        padded_image = irfft(fft_image, padded_size[1])
+        # Multiply in frequency domain
+        fft_image .*= fft_kernel
+
+        # Inverse FFT
+        mul!(padded_image, plan_inv, fft_image)
 
         # Remove padding
-        image = padded_image[(pad+1):(end-pad)]
-        return image
+        blurred_image .= padded_image[(pad+1):(end-pad)]
 
-    elseif length(image_dimensions) == 2    # 2D Images
-        # Create Kernel
-        kernel = ones(Float32, kernel_size, kernel_size) / (kernel_size^2)
+    elseif length(image_dimensions) == 2   # 2D Images
 
         # Pad image and kernel to match convolution product
         pad = div(kernel_size, 2)
         padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad)
 
-        padded_image = zeros(Float32, padded_size)
         padded_image[1+pad:end-pad, 1+pad:end-pad] .= Float32.(image)
 
-        padded_kernel = zeros(Float32, padded_size)
+        kernel .= 1/(kernel_size^2)
         padded_kernel[1:kernel_size, 1:kernel_size] .= kernel
-        padded_kernel = circshift(padded_kernel, (-pad, -pad))
+        padded_kernel .= circshift(padded_kernel, (-pad, -pad))
 
-        # Calculate real FFTs of image and kernel
-        fft_image = rfft(padded_image)
-        fft_kernel = rfft(padded_kernel)
+        # Forward FFTs
+        mul!(fft_image,  plan_fwd_image, padded_image)
+        mul!(fft_kernel, plan_fwd_kernel, padded_kernel)
 
-        # Calculate convolution via the real reverse FFT
-        fft_image .*= fft_kernel    # same array for lower memory usage
-        padded_image = irfft(fft_image, padded_size[2])
+        # Multiply in frequency domain
+        fft_image .*= fft_kernel
+
+        # Inverse FFT
+        mul!(padded_image, plan_inv, fft_image)
 
         # Remove padding
-        image = padded_image[(pad+1):(end-pad), (pad+1):(end-pad)]
-        return image
+        blurred_image .= padded_image[(pad+1):(end-pad), (pad+1):(end-pad)]
 
     elseif length(image_dimensions) == 3    # 3D Images
-        # Create Kernel
-        kernel = ones(Float32, kernel_size, kernel_size, kernel_size) / (kernel_size^3)
 
         # Pad image and kernel to match convolution product
         pad = div(kernel_size, 2)
         padded_size = (image_dimensions[1] + 2*pad, image_dimensions[2] + 2*pad, image_dimensions[3] + 2*pad)
 
-        padded_image = zeros(Float32, padded_size)
         padded_image[1+pad:end-pad, 1+pad:end-pad, 1+pad:end-pad] .= Float32.(image)
 
-        padded_kernel = zeros(Float32, padded_size)
+        kernel .= 1/(kernel_size^3)
         padded_kernel[1:kernel_size, 1:kernel_size, 1:kernel_size] .= kernel
-        padded_kernel = circshift(padded_kernel, (-pad, -pad, -pad))
+        padded_kernel .= circshift(padded_kernel, (-pad, -pad, -pad))
 
-        # Calculate real FFTs of image and kernel
-        fft_image = rfft(padded_image)
-        fft_kernel = rfft(padded_kernel)
+        # Forward FFTs
+        mul!(fft_image,  plan_fwd_image, padded_image)
+        mul!(fft_kernel, plan_fwd_kernel, padded_kernel)
 
-        # Calculate convolution via the real reverse FFT
-        fft_image .*= fft_kernel    # same array for lower memory usage
-        padded_image = irfft(fft_image, padded_size[3])
+        # Multiply in frequency domain
+        fft_image .*= fft_kernel
+
+        # Inverse FFT
+        mul!(padded_image, plan_inv, fft_image)
 
         # Remove padding
-        image = padded_image[(pad+1):(end-pad), (pad+1):(end-pad), (pad+1):(end-pad)]
-        return image
+        blurred_image .= padded_image[(pad+1):(end-pad), (pad+1):(end-pad), (pad+1):(end-pad)]
     else
         error("Image dimensions not supported")
     end
--- test.jl
+++ test.jl
@@ -4,47 +4,44 @@ using BenchmarkTools
 using .BoxBlur
 
 # Set image and size of kernel
-kernel_size = 31
+kernel_size = 3
 image_size = 512
 
 img1 = rand(image_size)                         #1D
 img2 = rand(image_size, image_size)             #2D
 img3 = rand(image_size, image_size, image_size) #3D
 
-test_image = img1
-
-image = test_image
+image = img2
 
 # Speed test 
+
+#println("Original Image:")
+#println(image)
+
 print("Blur filter (convolution) time:")
-@btime blur_filter_conv($image, $kernel_size) 
-print("Blur filter (FFT convolution) time:")
-@btime blur_filter_conv_FFT($image, $kernel_size)
-
+(conv_kernel, conv_padded_image, conv_blurred_image) = blur_filter_conv_init(size(image), kernel_size)
+@btime blur_filter_conv($image, $conv_kernel, $conv_padded_image, $conv_blurred_image, $kernel_size)
 
 """
-image = test_image
-
 # Print the blurred image with convolution
-
-println("Original Image:")
-println(image)
 println("\nTesting blur filter (convolution)...")
-blurred_conv = blur_filter_conv(image, kernel_size)
-println(size(blurred_conv))
+println(size(conv_blurred_image))
 println("Blurred Image (Convolution):")
-println(blurred_conv)
-    
-    
-# Print the blurred image with FFT convolution
-    
-println("\nTesting blur_filter_conv_FFT...")
-blurred_fft = blur_filter_conv_FFT(image, kernel_size)
-println(size(blurred_fft))
-println("Blurred Image (FFT):")
-println(blurred_fft)
+println(conv_blurred_image)
 """
-    
 
+print("Blur filter (FFT convolution) time:")
+(fft_kernel, fft_padded_image, fft_padded_kernel, fft_blurred_image, image_fft, kernel_fft, plan_fwd_image, plan_fwd_kernel, plan_inv) = blur_filter_fft_init(size(image), kernel_size)
+@btime blur_filter_FFT($image, $fft_kernel, $fft_padded_image, $fft_padded_kernel, $fft_blurred_image, $kernel_size, 
+                                                            $image_fft, $kernel_fft, $plan_fwd_image, $plan_fwd_kernel, $plan_inv)
 
+"""                                                     
+# Print the blurred image with FFT convolution    
+println("\nTesting blur_filter_conv_FFT...")
+println(size(fft_blurred_image))
+println("Blurred Image (FFT):")
+println(fft_blurred_image)
+"""
 
+# Check if the results are similar
+@test isapprox(conv_blurred_image, fft_blurred_image; atol=1e-6, rtol=1e-5)
